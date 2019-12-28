a090eb2807a6960d49ff3ee8030827dc6f185408
Muhammad Luthfi Taqwim <39212340+iluthtaqwim@users.noreply.github.com>
2019-12-28 13:26:45 +0700

    Add database backup sql

---
+++ db_jejaring.sql
@@ -0,0 +1,31 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.15 
+MySQL - 5.0.45-community-nt : Database - db_jejaring
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `mahasiswa` */
+
+DROP TABLE IF EXISTS `mahasiswa`;
+
+CREATE TABLE `mahasiswa` (
+  `npm` varchar(10) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `tanggal_lahir` date default NULL,
+  `tempat_lahir` varchar(50) default NULL,
+  `jenis_kelamin` enum('men','woman') default NULL,
+  `tentang_saya` text,
+  PRIMARY KEY  (`npm`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `mahasiswa` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
